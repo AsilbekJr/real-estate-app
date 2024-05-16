2732526e4a9f5a4e47ad9917deb348beb79f1d07
Asilbek <jaoripovich@gmail.com>
2024-05-16 17:09:02 +0500

    create pages and install react-router-dom package

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignOut from "./Pages/SignOut";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+
 const App = () => {
-  return <div className="text-red-600">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
