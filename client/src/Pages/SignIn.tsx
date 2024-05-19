0bcbd3e12aac6b21dcecd554677f75642872b32c
Asilbek <jaoripovich@gmail.com>
2024-05-19 10:25:20 +0500

    add redux

--- client/src/Pages/SignIn.tsx
+++ client/src/Pages/SignIn.tsx
@@ -1,5 +1,89 @@
-const SignIn = () => {
-  return <div>SignIn</div>;
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../redux/reduxHook";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
+
+type FormData = {
+  [key: string]: string;
+};
+
+const SignIn: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  const [formData, setFormData] = useState<FormData>({});
+  const { error, isLoading } = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    try {
+      e.preventDefault();
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(signInFailure(error.message));
+      } else {
+        return;
+      }
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign in</h1>
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={isLoading as boolean}
+          className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? "Loading..." : "Sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account?</p>
+        <Link to={"/sign-in"} className="text-blue-700">
+          Sign up
+        </Link>
+      </div>
+      {error && <span className="text-red-600">{error}</span>}
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ client/src/redux/user/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type isLoadingType = null | boolean;
+
+const initialState = {
+  currentUser: null,
+  error: null,
+  isLoading: <isLoadingType>null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.isLoading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    },
+  },
+});
+
+export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
+export default userSlice.reducer;
